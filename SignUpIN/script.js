8acdba772f4f4de9ea3c8fa140158a26b8012848
Vishal <vishal.bhumca21@gmail.com>
2024-03-05 00:05:54 +0530

    Slow fade in Out Sign Up and Sign In

--- SignUpIN/script.js
+++ SignUpIN/script.js
@@ -7,18 +7,28 @@ let PassChange = document.querySelector(".headingPass");
 
 signInBtn.addEventListener("click", () => {
   nameField.style.maxHeight = "0";
-  title.innerHTML = "Sign In";
   signUpBtn.classList.add("disable");
   signInBtn.classList.remove("disable");
   underline.style.transform = "translateX(35px)";
-  PassChange.innerHTML = "Forget Password ?";
+  PassChange.innerHTML = "Forget Password?!";
+
+  title.style.opacity = "0";
+  setTimeout(() => {
+    title.innerHTML = "Sign In";
+    title.style.opacity = "1";
+  }, 800);
 });
 
 signUpBtn.addEventListener("click", () => {
   nameField.style.maxHeight = "60px";
-  title.innerHTML = "Sign Up";
+
   signUpBtn.classList.remove("disable");
   signInBtn.classList.add("disable");
   underline.style.transform = "translateX(0)";
   PassChange.innerHTML = "Password Suggestion!!";
+  title.style.opacity = "0";
+  setTimeout(() => {
+    title.innerHTML = "Sign Up";
+    title.style.opacity = "1";
+  }, 800);
 });
